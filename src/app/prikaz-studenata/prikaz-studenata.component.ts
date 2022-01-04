e0a9bc66cc112cb95b3710562e59204720df51d8
unknown <ajlagudzevic1100@gmail.com>
2022-01-04 15:50:04 +0100

    Update and delete student

--- src/app/prikaz-studenata/prikaz-studenata.component.ts
+++ src/app/prikaz-studenata/prikaz-studenata.component.ts
@@ -1,6 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {Student} from '../models/Student';
 import {StudentService} from '../service/student.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {error} from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-prikaz-studenata',
@@ -9,18 +12,83 @@ import {StudentService} from '../service/student.service';
 })
 export class PrikazStudenataComponent implements OnInit {
 
-  constructor(private studentService: StudentService) {
+  editForm: FormGroup;
+  editLoad = false;
+
+  constructor(private studentService: StudentService, protected router: Router, private fb: FormBuilder) {
   }
 
   studenti: Student[] = [];
+  selectedOne: Student = null;
 
   ngOnInit(): void {
     this.getStudents();
+    this.initEditForm();
   }
 
   getStudents() {
     this.studentService.getStudent().subscribe((stud: Student[]) => {
       this.studenti = stud;
     });
+  }
+
+  initEditForm() {
+    this.editForm = this.fb.group({
+      ime: ['', Validators.required],
+      prezime: ['', Validators.required],
+      indeks: ['', Validators.required],
+      adresa: ['', Validators.required],
+      pol: ['', Validators.required],
+      lozinka: ['', Validators.required]
+    });
+  }
+
+  selectOne(student: Student) {
+    this.selectedOne = student;
+    this.editForm.patchValue({
+      ime: this.selectedOne.ime,
+      prezime: this.selectedOne.prezime,
+      indeks: this.selectedOne.indeks,
+      adresa: this.selectedOne.adresa,
+      pol: this.selectedOne.pol,
+      lozinka: this.selectedOne.lozinka
+    });
+  }
+
+  get editS() {
+    return this.editForm.controls;
+  }
+
+  onUpdate() {
+    const stud = new Student(this.editS.ime.value, this.editS.prezime.value, this.editS.indeks.value, this.editS.adresa.value, this.editS.pol.value, this.editS.lozinka.value);
+    console.log(stud);
+    this.studentService.updateStudent(stud).subscribe(response => {
+        alert(JSON.stringify('Student je izmenjen!'));
+        console.log(response);
+        this.getStudents();
+      },
+      err => console.log(err)
+    );
+
+    this.editLoad = false;
+  }
+
+  deleteStudent(indeks: string) {
+    this.studentService.deleteStudent(indeks).subscribe(
+      response => {
+        alert('Student je izbrisat!');
+        console.log(response);
+      }, err => console.log(err)
+    );
+  }
+
+
+  deleteOne(id: string) {
+    this.studentService.deleteStudent(id).subscribe(
+      response => {
+        this.getStudents();
+        alert('Student je obrisan!');
+        console.log(response);
+      }, err => console.log(err));
   }
 }
